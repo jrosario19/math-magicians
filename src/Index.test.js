2272826a8791b0efe1d019a68b57acff54c5b3e3
Juan Rosario <jrosario19@hotmail.com>
2022-06-01 12:24:07 -0400

    Update App.js

--- src/Index.test.js
+++ src/Index.test.js
@@ -14,28 +14,6 @@ describe('Reder Home Page', () => {
     const navLinkCalculator = document.querySelector('[href="/calculator"]');
     expect(navLinkCalculator.innerHTML).toMatch(/Calculator/i);
   });
-  it('When key 5 is click, 5 is display in the Calculator', () => {
-    render(<Calculator />);
-    const key = document.getElementById('5');
-    const displayContainer = document.getElementById('output');
-    expect(key.innerHTML).toBe('5');
-    fireEvent.click(key, {target:{innerText: "5"}});
-    expect(displayContainer.innerHTML).toBe('5');
-  });
-  it('Test the user interaction summing 5 + 7', () => {
-    render(<Calculator />);
-    const key5 = screen.getByText('5');
-    const key7 = screen.getByText('7');
-    const plusKey = screen.getByText('+');
-    const equalKey = screen.getByText('=');
-    const displayContainer = document.querySelector('#output');
-    expect(key5.innerHTML).toBe('5');
-    expect(key7.innerHTML).toBe('7');
-    expect(plusKey.innerHTML).toBe('+');
-    fireEvent.click(key5,{target:{innerText: "5"}});
-    fireEvent.click(plusKey,{target:{innerText: "+"}});
-    fireEvent.click(key7,{target:{innerText: "7"}});
-    fireEvent.click(equalKey,{target:{innerText: "="}});
-    expect(displayContainer.innerHTML).toBe('12');
-  });
+  
+  
 });
